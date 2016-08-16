047dc2029ea8a7d656cbca29f1e1b4cb93492d36
hellomoto6 <carofermendez@hotmail.com>
2016-08-16 00:45:09 +0000

    [REF] openacademy: Remove datetime_eg field

--- openacademy/model/openacademy_session.py
+++ openacademy/model/openacademy_session.py
@@ -5,8 +5,7 @@ class Session(models.Model):
    _name = 'openacademy.session'                                                                                       
                                                                                                                        
    name = fields.Char(required=True)                                                                                   
-   start_date = fields.Date(default=fields.Date.today)      
-   datetime_eg = fields.Datetime(default=fields.Datetime.now)                                                                                    
+   start_date = fields.Date(default=fields.Date.today)                                                                                          
    duration = fields.Float(digits=(6, 2), help="Duration in days")                                                       
    seats = fields.Integer(string="Number of seats")       
    instructor_id = fields.Many2one('res.partner', string="Instructor",domain=['|',                                                                        
