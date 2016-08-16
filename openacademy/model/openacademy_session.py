05d21e6bcdc08f2256bdb6dbecc35c3e0eb42697
hellomoto6 <carofermendez@hotmail.com>
2016-08-16 00:23:56 +0000

    [REF] openacademy: Adding fields function

--- openacademy/model/openacademy_session.py
+++ openacademy/model/openacademy_session.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-                                                                                                
-from openerp import fields, models                                                                                     
+from openerp import api,fields, models                                                                                     
                                                                                                                        
 class Session(models.Model):                                                                                           
    _name = 'openacademy.session'                                                                                       
                                                                                                                        
    name = fields.Char(required=True)                                                                                   
-   start_date = fields.Date()                                                                                          
+   start_date = fields.Date(default=fields.Date.today())                                                                                          
    duration = fields.Float(digits=(6, 2), help="Duration in days")                                                       
    seats = fields.Integer(string="Number of seats")       
    instructor_id = fields.Many2one('res.partner', string="Instructor",domain=['|',                                                                        
@@ -17,3 +17,12 @@ class Session(models.Model):
 				ondelete='cascade', string="Course", required=True)  
   
    attendee_ids = fields.Many2many('res.partner', string="Attendees")  
+   taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')                                           
+                                                                                                                     
+   @api.one                                                                                                           
+   @api.depends('seats', 'attendee_ids')                                                                              
+   def _taken_seats(self):                                                                                            
+       if not self.seats:                                                                                             
+           self.taken_seats = 0                                                                                       
+       else:                                                                                                          
+           self.taken_seats = 100.0 * len(self.attendee_ids) / self.seats                                           
