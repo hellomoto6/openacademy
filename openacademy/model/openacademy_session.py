ed8854f5d54a9e95d100772293cae9354948951e
hellomoto6 <carofermendez@hotmail.com>
2016-08-22 18:58:07 -0300

    no message

--- openacademy/model/openacademy_session.py
+++ openacademy/model/openacademy_session.py
@@ -13,14 +13,16 @@ class Session(models.Model):
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
     instructor_id = fields.Many2one('res.partner', string="Instructor",
-                                    domain=['|',
+                                    domain= ['|',
                                              ("instructor", "=", True),
                                              ("category_id.name", "ilike", "Teacher"),
-                                           ])
+                                            ]
+                                    )
     course_id = fields.Many2one('openacademy.course',
-                                 ondelete='cascade',
-                                 string="Course",
-                                 required=True)
+                                    ondelete='cascade',
+                                    string="Course",
+                                    required=True
+                                )
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
     taken_seats = fields.Float(string="Taken seats",
                                compute='_taken_seats', store=True)
