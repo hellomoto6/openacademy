0b6b735e8e31f19e7deaec555d94590570c08767
hellomoto6 <carofermendez@hotmail.com>
2016-08-22 18:14:52 -0300

    no message

--- openacademy/model/openacademy_session.py
+++ openacademy/model/openacademy_session.py
@@ -14,11 +14,11 @@ class Session(models.Model):
     seats = fields.Integer(string="Number of seats")
     instructor_id = fields.Many2one('res.partner', string="Instructor",
                                     domain=['|',
-                                    ("instructor", "=", True),
-                                    ("category_id.name", "ilike", "Teacher"),
+                                           ("instructor", "=", True),
+                                           ("category_id.name", "ilike", "Teacher"),
                                     ])
     course_id = fields.Many2one('openacademy.course',
-                ondelete='cascade', string="Course", required=True)
+                                ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
     taken_seats = fields.Float(string="Taken seats",
                                compute='_taken_seats', store=True)
@@ -49,15 +49,15 @@ class Session(models.Model):
         if self.seats < 0:
             return {
                 'warning': {
-                'title': _("Incorrect 'seats' value"),
-                'message':_("The number of available seats may not be negative"),
+                    'title': _("Incorrect 'seats' value"),
+                    'message': _("The number of available seats may not be negative"),
                 },
             }
         if self.seats < len(self.attendee_ids):
             return {
                 'warning': {
-                'title': _("Too many attendees"),
-                'message':_("Increase seats or remove excess attendees"),
+                    'title': _("Too many attendees"),
+                    'message': _("Increase seats or remove excess attendees"),
                 },
             }
 
