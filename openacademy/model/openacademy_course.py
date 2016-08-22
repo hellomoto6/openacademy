ac5f6511117848a4a9ab44afa510d39f23cc6f13
hellomoto6 <carofermendez@hotmail.com>
2016-08-22 16:32:18 -0300

    no message

--- openacademy/model/openacademy_course.py
+++ openacademy/model/openacademy_course.py
@@ -14,32 +14,30 @@ class Course(models.Model):
     _name = 'openacademy.course'
 
     #  Field reserved to identified name rec
-    name = fields.Char(string='Title', required=True)  
+    name = fields.Char(string='Title', required=True)
     description = fields.Text(string='Description')
     responsible_id = fields.Many2one('res.users',
-                     		     ondelete='set null',
-				     string="Responsible", index=True)
+                                     ondelete='set null', 
+                                     string="Responsible", index=True)
     session_ids = fields.One2many('openacademy.session', 'course_id', string="Sessions")
 
 
     _sql_constraints = [
-        ('name_description_check',
-         'CHECK(name != description)',
+        ('name_description_check','CHECK(name != description)',
         _("The title of the course should not be the description")),
 
-        ('name_unique',
-         'UNIQUE(name)',
+        ('name_unique','UNIQUE(name)',
         _("The course title must be unique")),
     ]
 
-    @api.one  #  api.one send defaults params: cr, uid, id, context
+    @api.one  # api.one send defaults params: cr, uid, id, context
     def copy(self, default=None):
         # print "estoy pasando por la funcion heredada de copy en cursos"
         if default is None:
             default = {}
         # default['name'] = self.name + ' (copy)'
-        copied_count = self.search_count(
-            [('name', '=like', _(u"Copy of {}%").format(self.name))])
+        copied_count = self.search_count([
+	               ('name', '=like', _(u"Copy of {}%").format(self.name))])
         if not copied_count:
             new_name = _(u"Copy of {}").format(self.name)
         else:
--- openacademy/model/partner.py
+++ openacademy/model/partner.py
@@ -7,5 +7,5 @@ class Partner(models.Model):
     _inherit = 'res.partner'
 
     instructor = fields.Boolean(default=False)
-    session_ids = fields.Many2many('openacademy.session',
+    session_ids = fields.Many2many('openacademy.session', 
                                    string="Session as attendee",readonly=True)
