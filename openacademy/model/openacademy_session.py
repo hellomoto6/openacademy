1295c1be714e6b687efaf8ca5b6c8149679c51e8
hellomoto6 <carofermendez@hotmail.com>
2016-08-22 16:01:48 -0300

    no message

--- openacademy/model/openacademy_session.py
+++ openacademy/model/openacademy_session.py
@@ -13,14 +13,13 @@ class Session(models.Model):
     seats = fields.Integer(string="Number of seats")
     instructor_id = fields.Many2one('res.partner', string="Instructor",
                                    domain=['|',
-					("instructor", "=", True),
-					("category_id.name", "ilike", "Teacher"),
-				   ])
+                                   ("instructor", "=", True),
+                                   ("category_id.name", "ilike", "Teacher"),
+                                   ])
     course_id = fields.Many2one('openacademy.course',
         ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
-    taken_seats = fields.Float(string="Taken seats", compute='_taken_seats',
-                              store=True)
+    taken_seats = fields.Float(string="Taken seats", compute='_taken_seats',store=True)
     active = fields.Boolean(default=True)
     end_date = fields.Date(string="End Date", store=True,
         compute='_get_end_date', inverse='_set_end_date')
@@ -30,10 +29,10 @@ class Session(models.Model):
         string="Attendees count", compute='_get_attendees_count', store=True)
     color = fields.Integer()
     state = fields.Selection([
-                  	     ('draft', 'Draft'),
-			     ('confirmed', 'Confirmed'),
-			     ('done', 'Done'),
- 			    ], default='draft', readonly=True)
+                             ('draft', 'Draft'),
+                             ('confirmed', 'Confirmed'),
+                             ('done', 'Done'),
+                             ], default='draft', readonly=True)
 
     @api.one
     @api.depends('seats', 'attendee_ids')
